1910f4c9562798381f6ce6482a134367e025cc10
Alejo <istominalexey@ro.ru>
2025-07-10 19:24:44 +0300

    ESLint fixed

--- src/Character.js
+++ src/Character.js
@@ -3,12 +3,12 @@ export default class Character {
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
       throw new Error('Имя должно быть строкой от 2 до 10 символов');
     }
-    
+
     const validTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
     if (!validTypes.includes(type)) {
       throw new Error('Некорректный тип персонажа');
     }
-    
+
     this.name = name;
     this.type = type;
     this.health = 100;
